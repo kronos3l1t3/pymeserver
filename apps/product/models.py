8d040b6790ae008ceda49584f8359fd9352e6166
kronos <kronos3l1t3@gmail.com>
2022-01-11 00:14:45 -0500

    Images

--- apps/product/models.py
+++ apps/product/models.py
@@ -9,6 +9,10 @@ class Product(models.Model):
         """
     name = models.CharField(max_length=70, default="")
     type = models.ForeignKey(ProductType, on_delete=models.CASCADE, related_name="ProductType")
+    image = models.FilePathField(blank=False, null=False)
+    active = models.BooleanField(default=True)
+    description = models.TextField(max_length=2048, default="")
+    meta_text = models.TextField(max_length=2048, default="")
     created_at = models.DateTimeField(
         _('action time'),
         auto_now_add=True,
@@ -32,6 +36,8 @@ class Car(models.Model):
         This is the table of the products car (Services)
         """
     name = models.CharField(max_length=70, default="")
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name="products",)
+    serialized = models.TextField(max_length=4096, default="")
     created_at = models.DateTimeField(
         _('action time'),
         auto_now_add=True,
@@ -48,3 +54,27 @@ class Car(models.Model):
     class Meta:
         verbose_name = _('Carrito')
         verbose_name_plural = _('Carrito Compra')
+
+
+class Images(models.Model):
+    """
+        This is the table of images of products
+        """
+    name = models.CharField(max_length=70, default="")
+
+    created_at = models.DateTimeField(
+        _('action time'),
+        auto_now_add=True,
+        editable=False
+    )
+    updated_at = models.DateTimeField(
+        _('change time'),
+        auto_now=True
+    )
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = _('Imagen')
+        verbose_name_plural = _('Imagenes')
