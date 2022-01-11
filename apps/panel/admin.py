7f4567a08ce41fbe3ba8c981eee1dfc5ed7888d7
kronos <kronos3l1t3@gmail.com>
2022-01-10 23:56:15 -0500

    Change admin web headers

--- apps/panel/admin.py
+++ apps/panel/admin.py
@@ -3,3 +3,6 @@ from django.contrib.admin.models import LogEntry
 
 
 admin.site.register(LogEntry)
+admin.site.site_header = "Sitio web de Servivip"
+admin.site.site_title = "Portal de Servivip"
+admin.site.index_title = "Bienvenidos al portal de administración de Servivip"
